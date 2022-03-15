cd8d91ee9a1e4752193a1841abad7944d8e829f7
Luke Moore <12770383+lukewmoore@users.noreply.github.com>
2022-03-16 08:40:47 +1000

    Version bump

--- mct-lead-form.php
+++ mct-lead-form.php
@@ -5,7 +5,7 @@
  * Description: Include Lead Forms on your website.
  * Author: Pivotal Agency
  * Author URI: https://pivotal.agency
- * Version: 1.2.0
+ * Version: 1.3.4
  * Requires at least: 5.4
  * Tested up to: 5.4
  * Requires PHP: 7.2
