97a9769619c44304d1e4990764e9853fafaec592
Thomas Chuntic <tnchuntic@hotmail.com>
2023-12-14 14:27:51 +1100

    Adjusted version 1.3.6

--- mct-lead-form.php
+++ mct-lead-form.php
@@ -5,7 +5,7 @@
  * Description: Include Lead Forms on your website.
  * Author: Pivotal Agency
  * Author URI: https://pivotal.agency
- * Version: 1.3.4
+ * Version: 1.3.6
  * Requires at least: 5.4
  * Tested up to: 5.4
  * Requires PHP: 7.2
